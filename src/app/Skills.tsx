32b7f23221799a5a19467d29d2d8ec6045220267
Schwab, Oliver M. - SDSMT Student <oliver.schwab@sdsmt.edu>
2023-09-25 10:24:46 -0600

    Added skills

--- src/app/Skills.tsx
+++ src/app/Skills.tsx
@@ -13,25 +13,39 @@ export default function Skills() {
                 <div id = "header">
                     Languages
                 </div>
-                <div id = "content">
-                -  Python <br/>
-                -  C++<br/>
-                -  Java<br/>
-                -  JavaScript<br/>
-                -  HTML/CSS<br/>
-                -  SQL<br/>
-                </div>
+                <div id = "content" className="justify-center flex flex-wrap">
+                
+
+            {[
+["Python",85,"4 Years"],
+["C++",80,"3 Years"],
+['git', 80, '3 Years'],
+["JavaScript", 60, "1 Year"],
+["Next.Js", 50, "6 Months"],
+['Linux', 50, "3 Years"],
+['Docker', 40, "6 Months"]
+            ].map(function(element) {
+                    return (
+                        <div className="FlexContainer">
+                        <div>{element[0]}</div>
+                        <div className="flex justify-center">
+                            <ProgressBar value={element[1]} showValue={false} className="!h-10" id="progBar"/>
+                        </div>
+                        <div>
+                            {element[2]}
+                        </div>
+                    </div>
+                    )
+            })}
             </div>
+            </div>
+
             
 
         </div>
     )
 }
 
-/*<div className="FlexContainer border">
-                <div>Python</div>
-                <ProgressBar value={50} showValue={false} className="!h-10" id="progBar"/>
-                <div></div>
-            </div>*/
+
 
 
