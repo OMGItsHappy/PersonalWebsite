217d80ab74f1457b6383400736b0489a929553d2
Oliver <oliverschwab03@gmail.com>
2024-02-05 08:37:47 -0700

    Updaed education

--- src/app/(mainpage)/MainPageSections/Education.tsx
+++ src/app/(mainpage)/MainPageSections/Education.tsx
@@ -18,12 +18,13 @@ export default function Education() {
             <a href = "https://www.sdsmt.edu/" className="my-underline">South Dakota Mines</a>
             </div>
             <div id = "subheader">
-            B.S. Computer Science, Computational Statistics Minor<br></br>
+            B.S. Computer Science, Math Minor<br></br>
             Expected Graduation <span id= "bold">December 2024</span>
             </div>
             <div id= "content">
             3.5 GPA<br/>
-            Dean&apos;s List Spring 2023<br></br>
+            Dean&apos;s List Spring 2023<br/>
+            Dean&apos;s List Fall 2023<br/><br/>
 
             <div className={(showCourses 
                 ? "animate__animated animate__zoomIn animate-in" 
